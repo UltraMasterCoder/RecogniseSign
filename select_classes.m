db4750fb435191fa7e7d96e43576d814acfb2437
keilhackerj <josef.keilhacker@gmail.com>
2018-11-05 22:55:01 +0100

    save changes

--- select_classes.m
+++ select_classes.m
@@ -1,34 +1,56 @@
 
+import mlreportgen.dom.*;
 
-% Show image
+% Read image
 I = imread('DTUSignPhotos/DTUSigns003.jpg');
+% Split color components
+I_red   = I(:,:,1);
+I_green = I(:,:,2);
+I_blue  = I(:,:,3);
 
-% select sign
-DTUSROI = roipoly(I);
-imwrite(DTUSROI, 'DTUSROI.png');
-%DTUSROI = imread('DTUSROI.png');
-DTUSVals = double(I(DTUSROI));
+% select classes
+% DTUS_ROI = roipoly(I);
+% imwrite(DTUS_ROI, 'DTUSROI.png');
+DTUS_ROI = imread('DTUS_ROI.png');
+DTUS_Vals = double(I(DTUS_ROI));
+DTUS_red_vals = double(I_red(DTUS_ROI));
+DTUS_green_vals = double(I_green(DTUS_ROI));
+DTUS_blue_vals = double(I_blue(DTUS_ROI));
 
-% % show hist
-% imhist(I);
-Ired   = I(:,:,1);
-Igreen = I(:,:,2);
-Iblue  = I(:,:,3);
-DTUSredVals = double(Ired(DTUSROI));
-DTUSgreenVals = double(Igreen(DTUSROI));
-DTUSblueVals = double(Iblue(DTUSROI));
+% Background_ROI = roipoly(I);
+% imwrite(Background_ROI, 'Background_ROI.png');
+Background_ROI = imread('Background_ROI.png');
+Background_Vals = double(I(Background_ROI));
+Background_red_vals = double(I_red(Background_ROI));
+Background_green_vals = double(I_green(Background_ROI));
+Background_blue_vals = double(I_blue(Background_ROI));
 
-% inspect the combined histogram of the R, G, B values.
-figure;
-totVals = [DTUSredVals DTUSgreenVals DTUSblueVals];
-nbins = 255;
-hist(totVals,nbins);
-h = findobj(gca,'Type','patch');
-set(h(3),'FaceColor','r','EdgeColor','r','FaceAlpha',0.3,'EdgeAlpha',0.3);
-set(h(2),'FaceColor','g','EdgeColor','g','FaceAlpha',0.3,'EdgeAlpha',0.3);
-set(h(1),'FaceColor','b','EdgeColor','b','FaceAlpha',0.3,'EdgeAlpha',0.3);
-xlim([0 255]);
+class_names = ['DTU Sign', 'Background'];
 
+% ul = OrderedList({Text('item1'),'item 2',3});
+% 
+% all_red_vals{1} = DTUS_red_vals;
+% all_red_vals{2} = Background_red_vals;
+% 
+% all_green_vals{1} = DTUS_green_vals;
+% all_green_vals{2} = Background_green_vals;
+% 
+% all_blue_vals{1} = DTUS_blue_vals;
+% all_blue_vals{2} = Background_blue_vals;
+pixel_count = 0;
+for obj = [length(DTUS_red_vals), length(Background_red_vals)]
+    if pixel_count < obj
+        pixel_count = obj;
+    end
+end
+all_red_vals = zeros(pixel_count, num_of_classes);
+all_red_vals(:,1) = DTUS_red_vals;
+all_red_vals(:,2) = Background_red_vals;
+% all_red_vals = [DTUS_red_vals DTUS_green_vals ];
+all_green_vals = [DTUS_green_vals Background_green_vals];
+all_blue_vals = [DTUS_blue_vals Background_green_vals];
+[N, num_of_classes] = size(all_red_vals);
+colormap = ['r', 'g', 'b'];%....
 % show histograms with each colour component
 figure;
 subplot(1,2,1);
@@ -36,34 +58,46 @@ imshow(I);
 title('original Image');
 
 subplot(3,2,2);
-hist(DTUSredVals,255);
+hist(all_red_vals,255);
 xlim([0 255]);
 title('red Values');
+h = findobj(gca,'Type','patch');
+for i = 1:num_of_classes
+    set(h(i),'FaceColor',colormap(i),'EdgeColor',colormap(i),'FaceAlpha',0.3,'EdgeAlpha',0.3);
+end
 
 subplot(3,2,4);
-hist(DTUSgreenVals,255);
+hist(all_green_vals,255);
 xlim([0 255]);
 title('green Values');
+h = findobj(gca,'Type','patch');
+for i = 1:num_of_classes
+    set(h(i),'FaceColor',colormap(i),'EdgeColor',colormap(i),'FaceAlpha',0.3,'EdgeAlpha',0.3);
+end
 
 subplot(3,2,6);
-hist(DTUSblueVals,255);
+hist(all_blue_vals,255);
 xlim([0 255]);
 title('blue Values');
+h = findobj(gca,'Type','patch');
+for i = 1:num_of_classes
+    set(h(i),'FaceColor',colormap(i),'EdgeColor',colormap(i),'FaceAlpha',0.3,'EdgeAlpha',0.3);
+end
 
-% RGB Threshold
-% TODO: This can be done better
-ISigns = Ired > 150 & Ired < 200  & Igreen > 45 & Igreen < 80 & Iblue > 60 & Iblue < 100;
-
-% TODO: Here should be a lot of intelligent code to extract signs
-% For example morphological operations and BLOB analysis
-
-% Binary operations to remove small objects
-se = strel('disk',3);
-Iopen = imopen(ISigns, se);
-
-% Blob extraction
-L8 = bwlabel(Iopen,8);
-
-% Return the raw BLOBS
-LabelMap = L8;
+% % RGB Threshold
+% % TODO: This can be done better
+% ISigns = Ired > 150 & Ired < 200  & Igreen > 45 & Igreen < 80 & Iblue > 60 & Iblue < 100;
+% 
+% % TODO: Here should be a lot of intelligent code to extract signs
+% % For example morphological operations and BLOB analysis
+% 
+% % Binary operations to remove small objects
+% se = strel('disk',3);
+% Iopen = imopen(ISigns, se);
+% 
+% % Blob extraction
+% L8 = bwlabel(Iopen,8);
+% 
+% % Return the raw BLOBS
+% LabelMap = L8;
 
