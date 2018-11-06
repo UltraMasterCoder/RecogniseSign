0fef30677935474895932caad42d942908e91eb2
keilhackerj <josef.keilhacker@gmail.com>
2018-11-06 08:23:20 +0100

    finish class selection script

--- select_classes.m
+++ select_classes.m
@@ -3,16 +3,20 @@ import mlreportgen.dom.*;
 
 % Read image
 I = imread('DTUSignPhotos/DTUSigns003.jpg');
+[Ix, Iy] = size(I);
+max_size = Ix*Iy;
 % Split color components
 I_red   = I(:,:,1);
 I_green = I(:,:,2);
 I_blue  = I(:,:,3);
 
+% Specify number of classes used
+num_of_classes = 2;
+
 % select classes
 % DTUS_ROI = roipoly(I);
 % imwrite(DTUS_ROI, 'DTUSROI.png');
 DTUS_ROI = imread('DTUS_ROI.png');
-DTUS_Vals = double(I(DTUS_ROI));
 DTUS_red_vals = double(I_red(DTUS_ROI));
 DTUS_green_vals = double(I_green(DTUS_ROI));
 DTUS_blue_vals = double(I_blue(DTUS_ROI));
@@ -20,36 +24,24 @@ DTUS_blue_vals = double(I_blue(DTUS_ROI));
 % Background_ROI = roipoly(I);
 % imwrite(Background_ROI, 'Background_ROI.png');
 Background_ROI = imread('Background_ROI.png');
-Background_Vals = double(I(Background_ROI));
 Background_red_vals = double(I_red(Background_ROI));
 Background_green_vals = double(I_green(Background_ROI));
 Background_blue_vals = double(I_blue(Background_ROI));
 
-class_names = ['DTU Sign', 'Background'];
+class_names = ["DTU Sign", "Background"];
 
-% ul = OrderedList({Text('item1'),'item 2',3});
-% 
-% all_red_vals{1} = DTUS_red_vals;
-% all_red_vals{2} = Background_red_vals;
-% 
-% all_green_vals{1} = DTUS_green_vals;
-% all_green_vals{2} = Background_green_vals;
-% 
-% all_blue_vals{1} = DTUS_blue_vals;
-% all_blue_vals{2} = Background_blue_vals;
-pixel_count = 0;
-for obj = [length(DTUS_red_vals), length(Background_red_vals)]
-    if pixel_count < obj
-        pixel_count = obj;
-    end
-end
-all_red_vals = zeros(pixel_count, num_of_classes);
-all_red_vals(:,1) = DTUS_red_vals;
-all_red_vals(:,2) = Background_red_vals;
-% all_red_vals = [DTUS_red_vals DTUS_green_vals ];
-all_green_vals = [DTUS_green_vals Background_green_vals];
-all_blue_vals = [DTUS_blue_vals Background_green_vals];
-[N, num_of_classes] = size(all_red_vals);
+all_red_vals = NaN(max_size, num_of_classes);
+all_red_vals(1:length(DTUS_red_vals),1) = DTUS_red_vals;
+all_red_vals(1:length(Background_red_vals),2) = Background_red_vals;
+
+all_green_vals = NaN(max_size, num_of_classes);
+all_green_vals(1:length(DTUS_green_vals),1) = DTUS_green_vals;
+all_green_vals(1:length(Background_green_vals),2) = Background_green_vals;
+
+all_blue_vals = NaN(max_size, num_of_classes);
+all_blue_vals(1:length(DTUS_blue_vals),1) = DTUS_blue_vals;
+all_blue_vals(1:length(Background_blue_vals),2) = Background_blue_vals;
+
 colormap = ['r', 'g', 'b'];%....
 % show histograms with each colour component
 figure;
@@ -65,6 +57,7 @@ h = findobj(gca,'Type','patch');
 for i = 1:num_of_classes
     set(h(i),'FaceColor',colormap(i),'EdgeColor',colormap(i),'FaceAlpha',0.3,'EdgeAlpha',0.3);
 end
+legend(class_names);
 
 subplot(3,2,4);
 hist(all_green_vals,255);
